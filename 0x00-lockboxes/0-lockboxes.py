ceb1a6e5a24601711dc7799b9bd4c2bab3caa6f2
Andres Felipe Escobar Cardona <57599404+AndresEscobarDev@users.noreply.github.com>
2021-12-09 18:21:18 -0500

    0x00. Lockboxes Task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__== "__main__":
+  pass
+
+def canUnlockAll(boxes):
+  length = len(boxes)
+
+  openBoxes = length * [False]
+  openBoxes[0] = True
+  keys = boxes[0]
+
+  for key in keys:
+    if openBoxes[key] is False:
+      openBoxes[key] = True
+      keys += boxes[key]
+
+  return False not in openBoxes
